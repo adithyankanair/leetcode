b5aa60a6ada84c2c5758c9dc0925b902482687ef
Amigo <adithyankalarickal003@gmail.com>
2026-04-02 22:33:44 +0530

    leetcode quest day 1

---
+++ concat_array.py
@@ -0,0 +1,5 @@
+def getConcatenation(nums):
+    return nums + nums
+'''
+given an list  return the array so that the new array have new array[i] = new array[i+n] = nums[i] where n is the length of old array
+'''
